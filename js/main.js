675f318a6538291c1555963cc1a8c9568a206924
Kirill <manfeed3@gmail.com>
2023-05-17 20:18:35 +0300

    Add adaptive to page

--- js/main.js
+++ js/main.js
@@ -27,3 +27,52 @@ function changeTab(e) {
 // buttons.forEach((elem) => elem.addEventListener("click", (e) => changeTab(e)));
 
 buttonsContainer.addEventListener("click", (e) => changeTab(e));
+
+const burger = document.querySelector(".header__burger");
+const menu = document.querySelector(".header__inner");
+const scrollingItems = document.querySelectorAll("[data-scroll-target]");
+const bag = document.querySelector(".header-footer__bag-on span");
+const bagBuy = document.querySelectorAll("[data-bag]");
+
+
+burger.addEventListener("click", () => {
+    menu.classList.toggle("header__menu-active");
+});
+
+const menuButtons = menu.querySelectorAll("a");
+
+function scrollingTo(e) {
+    e.preventDefault();
+    const scrollInfo = e.target.dataset.scroll;
+
+    // console.log(scrollInfo)
+    scrollingItems.forEach(elem => {
+        if (elem.dataset.scrollTarget === scrollInfo) {
+            console.log(elem.dataset.scrollTarget);
+            // elem.scrollIntoView({
+            //     block: "start",
+            //     behavior: "smooth"
+            // })
+            console.log(window.pageYOffset + elem.getBoundingClientRect().top);
+            window.scrollTo({
+                top: window.pageYOffset + elem.getBoundingClientRect().top - 20,
+                behavior: "smooth"
+            })
+
+            menu.classList.remove("header__menu-active");
+        }
+    })
+}
+
+menuButtons.forEach(el => {
+    el.addEventListener("click", (e) => {
+        scrollingTo(e)
+    })
+})
+
+
+bagBuy.forEach(elem => elem.addEventListener("click", (e) => {
+    const previous = +bag.textContent;
+    bag.textContent = previous + 1;
+}
+)) 
